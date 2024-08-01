f7948a6692fbf5f9db74df52343ac9a44b1976f2
mariosknl <mariosknl@outlook.com>
2024-08-01 11:58:40 +0300

    live features with Floating Comments

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -14,8 +14,9 @@ import Loader from "./Loader";
 const CollaborativeRoom = ({
 	roomId,
 	roomMetadata,
+	users,
+	currentUserType,
 }: CollaborativeRoomProps) => {
-	const currentUserType = "editor";
 	const [editing, setEditing] = useState(false);
 	const [loading, setLoading] = useState(false);
 	const [documentTitle, setDocumentTitle] = useState(roomMetadata.title);
@@ -122,7 +123,7 @@ const CollaborativeRoom = ({
 							</SignedIn>
 						</div>
 					</Header>
-					<Editor />
+					<Editor roomId={roomId} currentUserType={currentUserType} />
 				</div>
 			</ClientSideSuspense>
 		</RoomProvider>
