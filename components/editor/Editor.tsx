21c07d2c26f802cd4ea4d540753143de6dea580a
mariosknl <mariosknl@outlook.com>
2024-08-01 12:03:20 +0300

    Sticky Comments

--- components/editor/Editor.tsx
+++ components/editor/Editor.tsx
@@ -21,6 +21,7 @@ import Loader from "../Loader";
 
 import FloatingToolbarPlugin from "./plugins/FloatingToolbarPlugin";
 import { useThreads } from "@liveblocks/react/suspense";
+import Comments from "../Comments";
 
 // Catch any errors that occur during Lexical updates and log them
 // or throw them as needed. If you don't throw them, Lexical will
@@ -80,6 +81,7 @@ export function Editor({
 					<LiveblocksPlugin>
 						<FloatingComposer className="w-[350px]" />
 						<FloatingThreads threads={threads} />
+						<Comments />
 					</LiveblocksPlugin>
 				</div>
 			</div>
